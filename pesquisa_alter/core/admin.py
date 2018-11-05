7ee2a29f135c4a30a6589f3b8a7e77def3eb0cc3
rg3915 <rg3915@yahoo.com.br>
2018-11-05 00:16:17 -0200

    Inserindo Questions e Pesquisa no Admin

--- pesquisa_alter/core/admin.py
+++ pesquisa_alter/core/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Questions, Pesquisa
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'email', 'phone','cdalterdata')
-    search_fields = ('cdalterdata','name', 'email')
+    list_display = ('__str__', 'email', 'phone', 'cdalterdata')
+    search_fields = ('cdalterdata', 'name', 'email')
     #list_filter = ('gender',)
+
+
+@admin.register(Questions)
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = ('__str__', )
+
+
+@admin.register(Pesquisa)
+class PesquisaAdmin(admin.ModelAdmin):
+    list_display = ('__str__', )
